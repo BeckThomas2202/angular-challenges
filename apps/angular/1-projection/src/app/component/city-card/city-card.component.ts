358a0a9a8efde74df1a8dc0e64aa9c07dea44daf
Kouristoll <kouristoll@hotmail.be>
2024-11-01 09:01:36 +0100

    refactor: solution 1

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { City } from '../../model/city.model';
+import { CardRowDirective } from '../../ui/card/card-row.directive';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card
+      (addButtonClicked)="addCity()"
+      [list]="cities"
+      class="bg-blue-100">
+      <img src="assets/img/city.png" alt="city picture" width="200px" />
+      <ng-template [cardRow]="cities" let-city>
+        <app-list-item
+          (deleteButtonClicked)="deleteCity(city.id)"
+          [name]="city.name" />
+      </ng-template>
+    </app-card>
+  `,
   standalone: true,
-  imports: [],
+  imports: [CardRowDirective, ListItemComponent, CardComponent],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  cities: City[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private readonly store: CityStore,
+    private readonly http: FakeHttpService,
+  ) {}
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((cities) => this.store.addAll(cities));
+    this.store.cities$.subscribe((cities) => (this.cities = cities));
+  }
+
+  public addCity() {
+    this.store.addOne(randomCity());
+  }
+
+  public deleteCity(id: number) {
+    this.store.deleteOne(id);
+  }
 }
--- apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,40 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
 import { Teacher } from '../../model/teacher.model';
+import { CardRowDirective } from '../../ui/card/card-row.directive';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-teacher-card',
   template: `
     <app-card
       [list]="teachers"
-      [type]="cardType"
-      customClass="bg-light-red"></app-card>
+      class="bg-red-100"
+      (addButtonClicked)="addTeacher()">
+      <img src="assets/img/teacher.png" width="200px" />
+      <ng-template [cardRow]="teachers" let-teacher>
+        <app-list-item
+          (deleteButtonClicked)="deleteTeacher(teacher.id)"
+          [name]="teacher.firstName" />
+      </ng-template>
+    </app-card>
   `,
-  styles: [
-    `
-      ::ng-deep .bg-light-red {
-        background-color: rgba(250, 0, 0, 0.1);
-      }
-    `,
-  ],
+  styles: [``],
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, CardRowDirective, ListItemComponent],
 })
 export class TeacherCardComponent implements OnInit {
   teachers: Teacher[] = [];
-  cardType = CardType.TEACHER;
 
   constructor(
-    private http: FakeHttpService,
-    private store: TeacherStore,
+    private readonly http: FakeHttpService,
+    private readonly store: TeacherStore,
   ) {}
 
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
-
     this.store.teachers$.subscribe((t) => (this.teachers = t));
+  }
+
+  public addTeacher() {
+    this.store.addOne(randTeacher());
+  }
+
+  public deleteTeacher(id: number) {
+    this.store.deleteOne(id);
   }
 }
